5f627cef83955ffee6b856463b585f7278758e76
Bizon <v.stovpovyi@gmail.com>
2025-05-11 02:22:59 -0500

    added some comments, removed debug info

--- js/scrips.js
+++ js/scrips.js
@@ -8,17 +8,16 @@ async function tryToFindLocality(localityName){
     const response = await fetch(apiUrl);
     var respinseAsJson = await response.json();
     
-    var localities = document.getElementById("localities");    
+    var localities = document.getElementById("localities");
+    // clear privious results    
     while(localities.hasChildNodes())
     {        
         localities.childNodes[0].remove();
     }
         
     try
-    {        
-        console.log(JSON.stringify(respinseAsJson.results));
+    {   
         respinseAsJson.results.forEach(locality => {
-            console.log(locality.country + locality.admin1 + locality.name);            
             localities.appendChild(new Option(locality.country + ' / ' + locality.admin1 + ' / ' + locality.name), locality);
         })
 
@@ -28,7 +27,6 @@ async function tryToFindLocality(localityName){
         var targetLocality = respinseAsJson.results[0];
         document.getElementById("latitude").value = targetLocality.latitude;
         document.getElementById("longitude").value = targetLocality.longitude;
-        
     }
     catch(e)
     {
@@ -37,8 +35,7 @@ async function tryToFindLocality(localityName){
 }
 
 function searchButtonClick(){
-    var localityName = document.getElementById("searchField").value;
-    alert(localityName);
+    var localityName = document.getElementById("searchField").value;    
     tryToFindLocality(localityName);
 }
 
